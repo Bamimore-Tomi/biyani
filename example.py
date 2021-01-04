56160ec410ad898c48cdb61289735499185f429e
unknown <tomibami2020@gmail.com>
2021-01-04 01:23:21 +0100

    added an example

--- example.py
+++ example.py
@@ -1,3 +1,17 @@
 import requests
+import base64
 
-req = requests.get()
+b = {
+    'callback':'http://56400c5508ca.ngrok.io',
+    'address':'TB4zCj53RrzHBVwZBsaaaSHQKiixuNwW7D'
+}
+url = "https://biyani.herokuapp.com​/trx/create"
+req = requests.get(url,params=b).json()
+new_wallet_address = req.get('output_address')
+print(new_wallet_address)
+##Get QR code string to bytes
+a = req.get('qr_code').encode()
+png_btyes =  base64.b64decode(a)
+print(png_btyes)
+with open('test.png','wb') as f:
+    f.write(png_btyes)
